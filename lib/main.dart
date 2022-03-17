0aa16136eaf99101bcd65075b42d0f51a2c13653
okatomo <kaguratomato1@gmail.com>
2022-03-17 22:46:23 +0900

    text

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       body: Container(
         color: Colors.red,
+        child: Text('helloWorld'),
       ),
     );
   }
