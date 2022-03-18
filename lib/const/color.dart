64c9e2bdd782343f4e6d6491ee687f8db43003b8
Hiroaki Ueda <hiroakiueda2000@gmail.com>
2022-03-18 23:49:07 +0900

    main編集した

--- lib/const/color.dart
+++ lib/const/color.dart
@@ -14,3 +14,55 @@ class HexColor extends Color {
 }
 
 final Color color1 = HexColor("#5E17EB");
+
+MaterialColor buildMaterialColor(Color color) {
+  List strengths = <double>[.05];
+  Map<int, Color> swatch = {};
+  final int r = color.red, g = color.green, b = color.blue;
+
+  for (int i = 1; i < 10; i++) {
+    strengths.add(0.1 * i);
+  }
+  strengths.forEach((strength) {
+    final double ds = 0.5 - strength;
+    swatch[(strength * 1000).round()] = Color.fromRGBO(
+      r + ((ds < 0 ? r : (255 - r)) * ds).round(),
+      g + ((ds < 0 ? g : (255 - g)) * ds).round(),
+      b + ((ds < 0 ? b : (255 - b)) * ds).round(),
+      1,
+    );
+  });
+  return MaterialColor(color.value, swatch);
+}
+
+const MaterialColor customSwatch = MaterialColor(
+  0xFF5E17EB,
+  <int, Color>{
+    50: Color(0xFFECE3FD),
+    100: Color(0xFFCFB9F9),
+    200: Color(0xFFAF8BF5),
+    300: Color(0xFF8E5DF1),
+    400: Color(0xFF763AEE),
+    500: Color(0xFF5E17EB),
+    600: Color(0xFF5614E9),
+    700: Color(0xFF4C11E5),
+    800: Color(0xFF420DE2),
+    900: Color(0xFF3107DD),
+  },
+);
+
+const MaterialColor customSwatch2 = MaterialColor(
+  0xFFA4C639,
+  <int, Color>{
+    50: Color(0xFFF4F8E7),
+    100: Color(0xFFE4EEC4),
+    200: Color(0xFFD2E39C),
+    300: Color(0xFFBFD774),
+    400: Color(0xFFB2CF57),
+    500: Color(0xFFA4C639),
+    600: Color(0xFF9CC033),
+    700: Color(0xFF92B92C),
+    800: Color(0xFF89B124),
+    900: Color(0xFF78A417),
+  },
+);
--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Fake Notification',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: customSwatch,
       ),
       home: const MyHomePage(),
     );
@@ -34,18 +34,241 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: color1,
-      body: Padding(
-        padding: const EdgeInsets.only(top: 70),
-        child: Center(
-          child: Column(
-            children: const [
-              Text(
-                '偽通知を作る',
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold,),
-              ),
-            ],
+      appBar: AppBar(
+        backgroundColor: Colors.white24,
+        centerTitle: false,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        title: const Text(
+          'ようこそ！',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
           ),
+        ),
+      ),
+      //backgroundColor: customSwatch[600],
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 30, 10, 10),
+        child: Column(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              color: Colors.grey[200],
+              child: const Padding(
+                padding: EdgeInsets.only(left: 15),
+                child: Text(
+                  '偽通知を作る',
+                  style: TextStyle(
+                    // decoration: TextDecoration.underline,
+                    // decorationStyle: TextDecorationStyle.double,
+                    decorationColor: customSwatch,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              color: Colors.grey[200],
+              child: GridView.count(
+                shrinkWrap: true,
+                // gridDelegate:
+                //     const SliverGridDelegateWithFixedCrossAxisCount(
+                //   crossAxisCount: 2,
+                // ),
+                childAspectRatio: 1.3,
+                crossAxisCount: 2,
+                children: [
+                  GestureDetector(
+                    onTap: () {},
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      elevation: 20,
+                      shadowColor: customSwatch,
+                      color: Colors.grey,
+                      child: Row(
+                        children:  [
+                          const VerticalDivider(
+                            color: customSwatch,
+                            thickness: 3,
+                            width: 30,
+                            indent: 30,
+                            endIndent: 30,
+                          ),
+                          Image.asset('assets/line.png'),
+                          const SizedBox(width: 8),
+                          const Text(
+                            "ライン",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      elevation: 20,
+                      shadowColor: customSwatch,
+                      color: Colors.grey,
+                      child: Row(
+                        children: [
+                          const VerticalDivider(
+                            color: customSwatch,
+                            thickness: 3,
+                            width: 30,
+                            indent: 30,
+                            endIndent: 30,
+                          ),
+                          Image.asset('assets/insta.png'),
+                          const SizedBox(width: 8),
+                          const Text(
+                            "インスタ",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      elevation: 20,
+                      shadowColor: customSwatch,
+                      color: Colors.grey,
+                      child: Row(
+                        children: const [
+                          VerticalDivider(
+                            color: customSwatch,
+                            thickness: 3,
+                            width: 30,
+                            indent: 30,
+                            endIndent: 30,
+                          ),
+                          Text(
+                            "Line",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      elevation: 20,
+                      shadowColor: customSwatch,
+                      color: Colors.grey,
+                      child: Row(
+                        children: const [
+                          VerticalDivider(
+                            color: customSwatch,
+                            thickness: 3,
+                            width: 30,
+                            indent: 30,
+                            endIndent: 30,
+                          ),
+                          Text(
+                            "Line",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      elevation: 20,
+                      shadowColor: customSwatch,
+                      color: Colors.grey,
+                      child: Row(
+                        children: const [
+                          VerticalDivider(
+                            color: customSwatch,
+                            thickness: 3,
+                            width: 30,
+                            indent: 30,
+                            endIndent: 30,
+                          ),
+                          Text(
+                            "Line",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  // GestureDetector(
+                  //   onTap: () {},
+                  //   child: const Card(
+                  //     color: customSwatch,
+                  //     child: Text("Line",
+                  //         style: TextStyle(
+                  //             color: Colors.black,
+                  //             fontSize: 30,
+                  //             fontWeight: FontWeight.bold)),
+                  //   ),
+                  // ),
+                  // GestureDetector(
+                  //   onTap: () {},
+                  //   child: const Card(
+                  //     color: customSwatch,
+                  //     child: Text("Line",
+                  //         style: TextStyle(
+                  //             color: Colors.black,
+                  //             fontSize: 30,
+                  //             fontWeight: FontWeight.bold)),
+                  //   ),
+                  // ),
+                  // GestureDetector(
+                  //   onTap: () {},
+                  //   child: const Card(
+                  //     color: customSwatch,
+                  //     child: Text("Line",
+                  //         style: TextStyle(
+                  //             color: Colors.black,
+                  //             fontSize: 30,
+                  //             fontWeight: FontWeight.bold)),
+                  //   ),
+                  // ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
